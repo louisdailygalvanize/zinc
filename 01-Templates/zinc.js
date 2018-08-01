0effc57cc6e27a61daf65391df96c1d4da1f35a3
louisdailygalvanize <louis.daily@galvanize.com>
2018-08-01 13:20:11 -0500

    Completed creating elements from dynamic data.

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -8,14 +8,25 @@
         let userList = document.getElementById('z-user-list');
         for (let i = 0; i < results.length; i++) {
             let user = results[i];
+            let firstName = capitalize(user.name.first);
+            let lastName = capitalize(user.name.last);
+            let city = capitalize(user.location.city);
+            let state = capitalize(user.location.state);
+
             userList.insertAdjacentHTML('beforeend', `
             <li class="user">
-                <img class="user-photo" src="${user.picture.medium}" alt="Photo of ${user.name.first} ${user.name.last}">
-                <div class="user-name">${user.name.first} ${user.name.last}</div>
-                <div class="user-location">${user.location.city}, ${user.location.state}</div>
+                <img class="user-photo" src="${user.picture.medium}" alt="Photo of ${firstName} ${lastName}">
+                <div class="user-name">${firstName} ${lastName}</div>
+                <div class="user-location">${city}, ${state}</div>
                 <div class="user-email">${user.email}</div>
             </li>`);
         }
+    }
+
+    function capitalize(string) {
+        let words = string.split(' ');
+        let capWords = words.map(word => word[0].toUpperCase() + word.slice(1));
+        return capWords.join(' ');
     }
 
     function init() {
@@ -25,4 +36,5 @@
     }
 
     document.addEventListener('DOMContentLoaded', init);
+
 })();
